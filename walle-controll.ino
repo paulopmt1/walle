3254936d9950bb922e5b1038a4282ceb5eb6ca44
Paulo Marcos Trentin <paulo@paulotrentin.com.br>
2023-10-28 15:50:39 -0300

    Added support to head left and right movements

--- walle-controll.ino
+++ walle-controll.ino
@@ -1,7 +1,7 @@
 #include <Servo.h> 
 
 /**
- * Head direction pins and motors
+ * Head UpDown direction pins and motors
  */
 int headUpDownDirectionSensorPin = 2;
 volatile long headUpDownStartTime = 0;
@@ -12,6 +12,21 @@ const byte headUpDownDirectionMotorRightPin = 10;
 const byte headUpDownDirectionMotorLeftPin = 11;
 Servo headUpDownDirectionServoRight;
 Servo headUpDownDirectionServoLeft;
+int lastHeadUpDownValue = 0;
+long int lastHeadUpDownChangeTime = 0;
+
+/**
+ * Head RightLeft direction pins and motors
+ */
+int headRightLeftDirectionSensorPin = 18;
+volatile long headRightLeftStartTime = 0;
+volatile long headRightLeftCurrentTime = 0;
+volatile long headRightLeftPulses = 0;
+int headRightLeftPulseWidth = 0;
+const byte headRightLeftDirectionMotorPin = 9;
+Servo headRightLeftDirectionServo;
+int lastHeadRightLeftValue = 0;
+long int lastHeadRightLeftChangeTime = 0;
 
 /**
  * Back forward direction pins and motors
@@ -28,6 +43,7 @@ const byte backForwardL_enable = 52;
 const byte backForwardR_enable = 53;
 const byte MAX_SPEED_PWM_VALUE = 150;
 
+
 /**
  * Oil pump
  */
@@ -36,7 +52,7 @@ const byte oilPumpStartupSpeed = 200;
 const byte oilPumpOperationSpeed = 90;
 bool oilPumpIsOn = false;
 const int reduceOilPumpPressureAfterMS = 3000;
-const int stopOilPumpPressureAfterMS = 10000;
+const int stopOilPumpPressureAfterMS = 5000;
 Servo oilPump;
 unsigned long pumpStartTime;
 
@@ -49,6 +65,11 @@ void setup(){
   headUpDownDirectionServoLeft.attach(headUpDownDirectionMotorLeftPin);
   attachInterrupt(digitalPinToInterrupt(headUpDownDirectionSensorPin), headUpDownPulseTimer, CHANGE);
   
+  // Head right left
+  pinMode( headRightLeftDirectionSensorPin, INPUT_PULLUP );
+  headRightLeftDirectionServo.attach( headRightLeftDirectionMotorPin );
+  attachInterrupt(digitalPinToInterrupt( headRightLeftDirectionSensorPin ), headRightLeftPulseTimer, CHANGE);
+
   // Back forward
   pinMode(backForwardL_enable, OUTPUT);
   pinMode(backForwardR_enable, OUTPUT);
@@ -62,91 +83,24 @@ void setup(){
   oilPump.attach( oilPumpPWMPin, 1000, 2000 );
   oilPump.write(0);
 
-
-
   Serial.begin(115200);
   Serial.println("Wall-e started!");
 
-  delay(100);
-}
-
-void headUpDownPulseTimer() {
-  headUpDownCurrentTime  = micros();
-  if (headUpDownCurrentTime > headUpDownStartTime) {
-    headUpDownPulses = headUpDownCurrentTime - headUpDownStartTime;
-    headUpDownStartTime = headUpDownCurrentTime;
-  }
-}
-
-void backForwardPulseTimer() {
-  backForwardCurrentTime  = micros();
-  if (backForwardCurrentTime > backForwardStartTime) {
-    backForwardPulses = backForwardCurrentTime - backForwardStartTime;
-    backForwardStartTime = backForwardCurrentTime;
-  }
 }
 
 void loop() {
   modifyOilPumpSpeed();
-
-  if (headUpDownPulses < 2000){
-    headUpDownPulseWidth = headUpDownPulses;
-  }
-  if (backForwardPulses < 2000){
-    backForwardPulseWidth = backForwardPulses;
-  }
-
-  /**
-   * Head up down
-   */
-  int headUpDownMotorLeftValueAngle = constrain( map(headUpDownPulseWidth, 985, 1800, 140, 40), 45, 130) +2;
-  int headUpDownMotorRightValueAngle = constrain( map(headUpDownPulseWidth, 985, 1800, 40, 140), 50, 135);
-  headUpDownDirectionServoRight.write(headUpDownMotorRightValueAngle);
-  headUpDownDirectionServoLeft.write(headUpDownMotorLeftValueAngle);
-  // Serial.print( "Head puslse: ");
-  // Serial.print( headUpDownPulseWidth);
-  // Serial.print( " - left: ");
-  // Serial.print(headUpDownMotorLeftValueAngle);
-  // Serial.print( " - right: ");
-  // Serial.print(headUpDownMotorRightValueAngle);
-  // Serial.print( " - ");
-
-  /**
-   * Going forward and backwards
-   */
-  Serial.print( "Back forward puslse: ");
-  Serial.print( backForwardPulseWidth);
-  const int frontPWMValue = constrain( map( backForwardPulseWidth, 1490, 1980, 0, MAX_SPEED_PWM_VALUE ), 0, MAX_SPEED_PWM_VALUE );
-  const int backwardsPWMValue = constrain( map( backForwardPulseWidth, 1450, 990, 0, MAX_SPEED_PWM_VALUE ), 0, MAX_SPEED_PWM_VALUE );
-
-  if ( frontPWMValue > 5 || backwardsPWMValue > 5 ) {
-    lastSpeedChangeTime = millis();
-  }
-
-  if ( frontPWMValue > 0 ) {
-    digitalWrite( backForwardDirectionMotorRearPin, LOW );
-    analogWrite( backForwardDirectionMotorFrontPin, frontPWMValue );
-
-  } else {
-    digitalWrite( backForwardDirectionMotorFrontPin, LOW );
-    analogWrite( backForwardDirectionMotorRearPin, backwardsPWMValue );
-  }
-  Serial.print( " - frontPWMValue: ");
-  Serial.print( frontPWMValue );
-  Serial.print( " - backwardsPWMValue: ");
-  Serial.print( backwardsPWMValue );
-
-
-  Serial.println();
-
+  headUpDown();
+  headLeftRight();
+  goingBackForward();
 }
 
 
 void oilPumpOn() {
   if ( ! oilPumpIsOn ) {
-    oilPumpIsOn = true;
-    oilPump.write( oilPumpStartupSpeed );
-    pumpStartTime = millis();
+    // oilPumpIsOn = true;
+    // oilPump.write( oilPumpStartupSpeed );
+    // pumpStartTime = millis();
   }
 }
 
@@ -157,7 +111,7 @@ void oilPumpOff() {
 }
 
 void modifyOilPumpSpeed() {
-  if ( millis() > pumpStartTime + reduceOilPumpPressureAfterMS ) {
+  if ( oilPumpIsOn && millis() > pumpStartTime + reduceOilPumpPressureAfterMS ) {
     oilPump.write( oilPumpOperationSpeed );
   }
 
@@ -167,5 +121,133 @@ void modifyOilPumpSpeed() {
 
   if ( lastSpeedChangeTime + 50 > millis() ) {
     oilPumpOn();
+  }
+}
+
+/**
+ * Head up down
+ */
+void headUpDown() {
+  if (headUpDownPulses < 2000){
+    headUpDownPulseWidth = headUpDownPulses;
+  }
+
+  // Remove noise
+  int headUpDownPulseNormalized = round( headUpDownPulseWidth * 0.01);
+  int headUpDownPulseWidthReverse = map( headUpDownPulseNormalized, 10, 20, 20, 10 );
+  int headUpDownMotorLeftValueAngle = constrain( map(headUpDownPulseWidthReverse, 10, 20, 140, 40), 45, 130) +2;
+  int headUpDownMotorRightValueAngle = constrain( map(headUpDownPulseWidthReverse, 10, 20, 40, 140), 50, 135);
+ 
+  if ( lastHeadUpDownValue != headUpDownPulseNormalized ) {
+    lastHeadUpDownValue = headUpDownPulseNormalized;
+    headUpDownDirectionServoRight.attach(headUpDownDirectionMotorRightPin);
+    headUpDownDirectionServoLeft.attach(headUpDownDirectionMotorLeftPin);
+    headUpDownDirectionServoRight.write(headUpDownMotorRightValueAngle);
+    headUpDownDirectionServoLeft.write(headUpDownMotorLeftValueAngle);
+    lastHeadUpDownChangeTime = millis();
+  }
+
+  if ( millis() > lastHeadUpDownChangeTime + 500 ) {
+    headUpDownDirectionServoRight.detach();
+    headUpDownDirectionServoLeft.detach();
+  }
+
+  Serial.print( "Head puslse: ");
+  Serial.print( headUpDownPulseWidthReverse );
+  Serial.print( " - left: ");
+  Serial.print(headUpDownMotorLeftValueAngle);
+  Serial.print( " - right: ");
+  Serial.print(headUpDownMotorRightValueAngle);
+  Serial.print( " - ");
+  Serial.println();  
+}
+
+/**
+ * Head Right Left
+ */
+void headLeftRight() {
+  if (headRightLeftPulses < 2000){
+    headRightLeftPulseWidth = headRightLeftPulses;
+  }
+
+  // Remove noise
+  int headRightLeftPulseNormalized = round( headRightLeftPulseWidth * 0.01);
+  int headRightLeftPulseWidthReverse = map( headRightLeftPulseNormalized, 10, 20, 20, 10 );
+  int headRightLeftMotorValueAngle = constrain( map(headRightLeftPulseWidthReverse, 10, 20, 20, 160), 20, 160);
+ 
+  if ( lastHeadRightLeftValue != headRightLeftPulseNormalized ) {
+    lastHeadRightLeftValue = headRightLeftPulseNormalized;
+    headRightLeftDirectionServo.attach(headRightLeftDirectionMotorPin);
+    headRightLeftDirectionServo.write(headRightLeftMotorValueAngle);
+    lastHeadRightLeftChangeTime = millis();
+
+    // Serial.print( "Head Left Right pulse normalized: ");
+    // Serial.print( headRightLeftPulseNormalized );
+    // Serial.print( " - angle: ");
+    // Serial.print( headRightLeftMotorValueAngle);
+    // Serial.print( " - ");
+    // Serial.println();
+  }
+
+  if ( millis() > lastHeadRightLeftChangeTime + 500 ) {
+    headRightLeftDirectionServo.detach();
+  }
+
+}
+
+/**
+ * Going forward and backwards
+ */
+void goingBackForward() {
+  const int backForwardPulseWidthNormalized = round( backForwardPulseWidth * 0.01 );
+  const int frontPWMValue = constrain( map( backForwardPulseWidthNormalized, 15, 20, 0, MAX_SPEED_PWM_VALUE ), 0, MAX_SPEED_PWM_VALUE );
+  const int backwardsPWMValue = constrain( map( backForwardPulseWidthNormalized, 15, 10, 0, MAX_SPEED_PWM_VALUE ), 0, MAX_SPEED_PWM_VALUE );
+
+  if ( frontPWMValue > 10 || backwardsPWMValue > 10 ) {
+    lastSpeedChangeTime = millis();
+  }
+
+  if ( frontPWMValue > 0 ) {
+    digitalWrite( backForwardDirectionMotorRearPin, LOW );
+    analogWrite( backForwardDirectionMotorFrontPin, frontPWMValue );
+  } else {
+    digitalWrite( backForwardDirectionMotorFrontPin, LOW );
+    analogWrite( backForwardDirectionMotorRearPin, backwardsPWMValue );
+  }
+  // Serial.print( "Back forward puslse: ");
+  // Serial.print( backForwardPulseWidthNormalized );
+  // Serial.print( " - frontPWMValue: ");
+  // Serial.print( frontPWMValue );
+  // Serial.print( " - backwardsPWMValue: ");
+  // Serial.print( backwardsPWMValue );
+  // Serial.println();
+}
+
+
+
+/**
+ * INTERRUPTS
+ */
+void headUpDownPulseTimer() {
+  headUpDownCurrentTime  = micros();
+  if (headUpDownCurrentTime > headUpDownStartTime) {
+    headUpDownPulses = headUpDownCurrentTime - headUpDownStartTime;
+    headUpDownStartTime = headUpDownCurrentTime;
+  }
+}
+
+void headRightLeftPulseTimer() {
+  headRightLeftCurrentTime  = micros();
+  if (headRightLeftCurrentTime > headRightLeftStartTime) {
+    headRightLeftPulses = headRightLeftCurrentTime - headRightLeftStartTime;
+    headRightLeftStartTime = headRightLeftCurrentTime;
+  }
+}
+
+void backForwardPulseTimer() {
+  backForwardCurrentTime  = micros();
+  if (backForwardCurrentTime > backForwardStartTime) {
+    backForwardPulses = backForwardCurrentTime - backForwardStartTime;
+    backForwardStartTime = backForwardCurrentTime;
   }
 }
